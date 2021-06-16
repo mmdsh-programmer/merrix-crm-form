440c516ddc01e986f1076d85c08f610531d5ea41
Mohammad Shakeri <m-shakeri@merrix.local>
2021-06-16 10:54:59 +0430

    add time

--- gheir-hozoori.php
+++ gheir-hozoori.php
@@ -23,9 +23,10 @@ $clue = $_POST['clue'];
 $service_type = $_POST['service_type'];
 $lat = $_POST['lat'];
 $lng = $_POST['lng'];
+$today = date("Y-m-d");
 $duplicateLandLine = mysqli_query($conn, "select * from gheir_hozoori where landline='$landline' and landline<>''");
 $duplicatePhone = mysqli_query($conn, "select * from gheir_hozoori where phone='$phone' and phone<>''");
-$duplicateInstagram = mysqli_query($conn, "select * from gheir_hozoori where instagram='$instagram' and instagram<>''");
+$duplicateInstagram = $instagram != "" ? mysqli_query($conn, "select * from gheir_hozoori where instagram like '%" . $instagram . "%' and instagram<>''") : mysqli_query($conn, "select * from gheir_hozoori where instagram like '$instagram' and instagram<>''");
 $duplicateTelegram = mysqli_query($conn, "select * from gheir_hozoori where telegram='$telegram' and telegram<>''");
 $duplicateWebsite = mysqli_query($conn, "select * from gheir_hozoori where website='$website' and website<>''");
 if (mysqli_num_rows($duplicateLandLine) > 0) {
@@ -39,8 +40,8 @@ if (mysqli_num_rows($duplicateLandLine) > 0) {
 } else if (mysqli_num_rows($duplicateWebsite) > 0) {
     echo json_encode(array("statusCode" => 206));
 } else {
-    $sql = "INSERT INTO `gheir_hozoori`(`user_email`,`shop_name`,`landline`,`phone`,`province`,`city`,`area`,`address`,`job`,`instagram`,`telegram`,`description`,`website`,`clue`,`service_type`,`lat`,`lng`,`import_crm`) 
-        VALUES ('$email', '$store_name', '$landline', '$phone', '$province', '$city', '$area', '$address', '$job', '$instagram', '$telegram', '$description', '$website', '$clue', '$service_type', '$lat', '$lng', '0')";
+    $sql = "INSERT INTO `gheir_hozoori`(`user_email`, `shop_name`, `landline`, `phone`, `province`, `city`, `area`, `address`, `job`, `instagram`, `telegram`, `description`, `website`, `clue`, `service_type`, `lat`, `lng`, `time`, `import_crm`) 
+        VALUES ('$email', '$store_name', '$landline', '$phone', '$province', '$city', '$area', '$address', '$job', '$instagram', '$telegram', '$description', '$website', '$clue', '$service_type', '$lat', '$lng', '$today', '0')";
     if (mysqli_query($conn, $sql)) {
         echo json_encode(array("statusCode" => 200));
     } else {
